71120cfc4835061e5926795c0b167f7ea592da46
liwenjie <704552737@qq.com>
2018-09-03 09:38:10 +0800

    脚手架搭建完成

--- gulpfile.js
+++ gulpfile.js
@@ -26,7 +26,24 @@ gulp.task('minJs', function() {
     })
     //监听js
 gulp.task('watch', function() {
-    return gulp.watch('./src/js/**/*.js', gulp.series('minJs'))
+        return gulp.watch('./src/js/**/*.js', gulp.series('minJs'))
+    })
+    //搭建服务器
+gulp.task('devServer', function() {
+    return gulp.src('src')
+        .pipe(server({
+            port: 9090,
+            middleware: function(req, res, next) {
+                var pathname = require('url').parse(req.url).pathname;
+                console.log(pathname)
+                if (pathname === '/favicon.ico') {
+                    res.end('');
+                    return;
+                }
+                pathname = pathname === '/' ? '/index.html' : pathname;
+                res.end(require('fs').readFileSync(require('path').join(__dirname, 'src', pathname)))
+            }
+        }))
 })
 
 //整合任务
